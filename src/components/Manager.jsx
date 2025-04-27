293ac404aa7c717a6fd478fe43141eb18a424e93
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-04-27 09:09:53 +0500

    push

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,7 +4,9 @@ const Manager = () => {
   return (
     <>
     <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-    <div className="container mx-auto bg bg-slate-700 max-w-4xl">
+    <div className="bg-gray-800 mx-auto mycontainer">
+      <h1>PassOP</h1>
+        <p>Your own password manager</p>
 
     <div className="text-white flex flex-col p-4">
       <input className='rounded-full ' type="text" name="" id="" />
