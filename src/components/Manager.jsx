c185994eb7a2fc6564f0fa61e7780024a5b79cc7
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-04-29 00:11:45 +0500

    push

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,29 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Manager = () => {
   return (
     <>
-    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-    <div className="mycontainer">
-      <h1 className='text-4xl font-bold text-center'>
-      <span className='text-green-700'>&lt;</span>
-          <span className='text-white'>Pass</span>
-          <span className='text-green-700'>OP/&gt;</span>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="mycontainer">
+        <h1 className="text-4xl font-bold text-center">
+          <span className="text-green-700">&lt;</span>
+          <span className="text-white">Pass</span>
+          <span className="text-green-700">OP/&gt;</span>
         </h1>
-        <p className='text-green-900 text-lg text-center'>Your own password manager</p>
+        <p className="text-green-900 text-lg text-center">
+          Your own password manager
+        </p>
 
-    <div className="text-white flex flex-col p-4 gap-8">
-      <input className='rounded-full border border-green-700 w-full p-4 py-1' type="text" name="" id="" />
-      <div className="flex w-full justify-between gap-8">
-      <input className='rounded-full border border-green-700 w-full p-4 py-1' type="text" name="" id="" />
-      <input className='rounded-full border border-green-700 w-full p-4 py-1' type="text" name="" id="" />
-
+        <div className="text-white flex flex-col p-4 gap-8">
+          <input
+            className="rounded-full border border-green-700 w-full p-4 py-1"
+            type="text"
+            name=""
+            id=""
+          />
+          <div className="flex w-full justify-between gap-8">
+            <input
+              className="rounded-full border border-green-700 w-full p-4 py-1"
+              type="text"
+              name=""
+              id=""
+            />
+            <input
+              className="rounded-full border border-green-700 w-full p-4 py-1"
+              type="text"
+              name=""
+              id=""
+            />
+          </div>
+          <button className="flex justify-center items-center">
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+              colors="primary:white"
+            ></lord-icon>
+            Add Password
+          </button>
+        </div>
       </div>
-      <button>Add Password</button>
-    </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Manager
+export default Manager;
