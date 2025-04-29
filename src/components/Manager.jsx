de7af1c30f307fa51d864fe71d90010a23f7b17d
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-04-29 12:03:30 +0500

    push

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Manager = () => {
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      {/* <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#4caf50_100%)]"></div>
+
+
+
+
       <div className="mycontainer">
         <h1 className="text-4xl font-bold text-center">
           <span className="text-green-700">&lt;</span>
@@ -14,7 +19,7 @@ const Manager = () => {
           Your own password manager
         </p>
 
-        <div className="text-white flex flex-col p-4 gap-8">
+        <div className="text-white flex flex-col p-4 gap-8 items-center">
           <input
             className="rounded-full border border-green-700 w-full p-4 py-1"
             type="text"
@@ -35,11 +40,10 @@ const Manager = () => {
               id=""
             />
           </div>
-          <button className="flex justify-center items-center">
+          <button className="cursor-pointer text-black flex justify-center items-center bg-green-500 hover:bg-green-300 rounded-full px-4 py-2 w-fit">
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
-              colors="primary:white"
             ></lord-icon>
             Add Password
           </button>
