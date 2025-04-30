0c3bbb3acc0449ee1963eb1b4af4f4d6400ea099
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-04-30 12:22:01 +0500

    push

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,16 +1,42 @@
 import React from "react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Manager = () => {
-  const ref = useRef()
-  const showPassword = ()=>{
-    if(ref.current.src.includes("icons/eyecross.png")){
-      ref.current.src = "icons/eye.png"
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setpasswordArray] = useState([])
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    let passwordArray;
+    if(passwords){
+      setpasswordArray(JSON.parse(passwords))
     }
-    else{
-      ref.current.src = "icons/eyecross.png"
+    
+  }, [])
+  
+
+
+  const showPassword = () => {
+    if (ref.current.src.includes("icons/eyecross.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
     }
+  };
+
+  const savePassword = () => {
+    console.log(form)
+    passwordArray.push(form)
+    
+  };
+
+
+  const handleChange = (e) => {
+    setform({...form, [e.target.name]: e.target.value})
   }
+  
+
   return (
     <>
       {/* <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
@@ -27,35 +53,52 @@ const Manager = () => {
 
         <div className="text-white flex flex-col p-4 gap-8 items-center">
           <input
+            value={form.site}
+            onChange={handleChange}
             className="rounded-full border border-green-700 w-full p-4 py-1"
             type="text"
-            name=""
+            name="site"
             id=""
             placeholder="Enter website URL"
           />
           <div className="flex w-full justify-between gap-8">
             <input
+              value={form.username}
+              onChange={handleChange}
               className="rounded-full border border-green-700 w-full p-4 py-1"
               type="text"
-              name=""
+              name="username"
               id=""
               placeholder="Enter Username"
             />
             <div className="relative">
-
-            <input
-              className="rounded-full border border-green-700 w-full p-4 py-1"
-              type="text"
-              name=""
-              id=""
-              placeholder="Enter Password"
+              <input
+                value={form.password}
+                onChange={handleChange}
+                className="rounded-full border border-green-700 w-full p-4 py-1"
+                type="text"
+                name="password"
+                id=""
+                placeholder="Enter Password"
               />
-              <span className="absolute right-[3px] top-[4px] cursor-pointer px-2" onClick={showPassword}>
-                <img ref={ref} className="p-1" src="icons/eye.png" alt="eye" width={26} />
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer px-2"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  src="icons/eye.png"
+                  alt="eye"
+                  width={26}
+                />
               </span>
-              </div>
+            </div>
           </div>
-          <button onClick={savePassword} className="cursor-pointer text-black flex justify-center items-center bg-green-500 hover:bg-green-300 rounded-full px-6 py-2 w-fit gap-3 shadow-md shadow-gray-400/30">
+          <button
+            onClick={savePassword}
+            className="cursor-pointer text-black flex justify-center items-center bg-green-500 hover:bg-green-300 rounded-full px-6 py-2 w-fit gap-3 shadow-md shadow-gray-400/30"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
