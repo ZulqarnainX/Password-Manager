c300d0fed71f03723907368d72e0039674a22497
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-05-13 03:36:20 +0500

    push

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
 import { ToastContainer, toast, Bounce } from "react-toastify";
+import { v4 as uuidv4 } from 'uuid';
 import "react-toastify/dist/ReactToastify.css";
 
 const Manager = () => {
