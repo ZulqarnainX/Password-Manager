87404e07ec65cc2e9f62aef8db8922de5ff0012f
Zulqarnain Rizvi <syed.zulqarnain557@gmail.com>
2025-05-19 10:51:16 +0500

    push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = ({ DarkMode, SetDarkMode }) => {
         ${DarkMode ? 'outline bg-black shadow-[0_0_8px_2px_rgba(156,163,175,0.5)]' : 'bg-[#63a9f6]  border-blue-500 outline outline-blue-200 shadow-lg shadow-blue-100'}`}
     >
       <div className="logo font-bold text-2xl">
-        <span className="text-green-700">&lt;</span>
+        <span className={` ${DarkMode ? 'text-green-700' : 'text-black'}`}>&lt;</span>
         Pass
-        <span className="text-green-700">OP/&gt;</span>
+        <span className={`${DarkMode ? 'text-green-700' : 'text-black'}`}>OP/&gt;</span>
       </div>
       <div className="flex justify-center items-center">
         Created with <img className="w-7 mx-2" src="icons/heart.png" alt="" /> by ZulqarnainX
